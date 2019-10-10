01cacb8437b49322c02a61438b72404bcb85de99
hunterkiddo <hunter.kiddo@hotmail.com>
2019-10-10 10:52:08 -0400

    functionality transfered to standalone functions
    code for functionality removed from render() function, and instead placed in two different standalone functions: one for organizing the data, and another for constructing the table

--- src/containers/home.js
+++ src/containers/home.js
@@ -69,71 +69,18 @@ export default class Home extends Component {
 		}
 		
 		this.Sponsors = [
-
             {
-
                 name: "Google",
-
                 photo: "https://www.shpeucf.com/wp-content/uploads/2019/08/Google-logo-e1565828559704.png",
-
                 link: "https://about.google/",
-
                 description: "Google does stuff"
-
             }
-
         ]
-}
-
-	renderOfficeHours(){
 	}
-
+	
     render() {
-		//------------------------------------------------------------------------------------------------//
 		let rows = []
-		let cell = []
-		//console.log(Object.keys(this.officeHourSchedule.dates))
-		let organizedData = []
-		let x = 0, y = 0, z = 0, count = 0, i = 0
-		let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-		let currentDay = days[0]
-		
-		while (count < this.officeHourSchedule.dates.length){
-			if(this.officeHourSchedule.dates[x].day == currentDay){
-				organizedData.push(this.officeHourSchedule.dates[x])
-				count++
-			}
-			x++
-			if(x >= this.officeHourSchedule.dates.length){ //all data has been cycled, array of currentDay is complete
-				x = 0 //reset dates counter
-				//addToScheduleTable(organizedData)
-				//--------------------------------//
-				cell = []
-				cell.push(<td> {currentDay} </td>) //day.charAt(0).toUpperCase() + day.slice(1).toLowerCase()
-				for(z = 0; z < organizedData.length; z++){
-					cell.push( <td align='left'> {organizedData[z].name} </td>)
-					for(i = 0; i < organizedData[z].time.length; i++){
-						if(i > 0){
-							rows.push(<tr> {cell} </tr>)
-							cell = []
-							cell.push(<td></td>)
-							cell.push(<td></td>)
-						}
-						cell.push( <td align='right'> {organizedData[z].time[i]} </td>)
-					}
-					rows.push(<tr> {cell} </tr>)
-					cell = []
-					cell.push(<td></td>)
-				}
-				//--------------------------------//
-				y++ //iterate current day
-				if(y < days.length){ //make sure y doesn't go over the number of days
-					currentDay = days[y] //set current day
-				}
-				organizedData = []
-			}
-		}
-		//------------------------------------------------------------------------------------------------//
+		rows = organizeSchedule(this.officeHourSchedule)
 			
         return (
             <div id='page'>
@@ -168,9 +115,53 @@ export default class Home extends Component {
 }
 
 function organizeSchedule(officeHourSchedule){
+	let rows = []
+	let cell = []
+	//console.log(Object.keys(this.officeHourSchedule.dates))
+	let organizedData = []
+	let x = 0, y = 0, count = 0
+	let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+	let currentDay = days[0]
+	
+	while (count < officeHourSchedule.dates.length){
+		if(officeHourSchedule.dates[x].day == currentDay){
+			organizedData.push(officeHourSchedule.dates[x])
+			count++
+		}
+		x++
+		if(x >= officeHourSchedule.dates.length){ //all data has been cycled, array of currentDay is complete
+			x = 0 //reset dates counter
+			rows = addToScheduleTable(organizedData, rows, currentDay)
+			y++ //iterate current day
+			if(y < days.length){ //make sure y doesn't go over the number of days
+				currentDay = days[y] //set current day
+			}
+			organizedData = []
+		}
+	}
+	return rows
 }
 
-function addToScheduleTable(organizedData){
+function addToScheduleTable(organizedData, rows, currentDay){
+	let cell = []
+	let i = 0, z=0
+	cell.push(<td> {currentDay} </td>) //day.charAt(0).toUpperCase() + day.slice(1).toLowerCase()
+	for(z = 0; z < organizedData.length; z++){
+		cell.push( <td align='left'> {organizedData[z].name} </td>)
+		for(i = 0; i < organizedData[z].time.length; i++){
+			if(i > 0){
+				rows.push(<tr> {cell} </tr>)
+				cell = []
+				cell.push(<td></td>)
+				cell.push(<td></td>)
+			}
+			cell.push( <td align='right'> {organizedData[z].time[i]} </td>)
+		}
+		rows.push(<tr> {cell} </tr>)
+		cell = []
+		cell.push(<td></td>)
+	}
+	return rows
 }
 
 const stylesheet = {
