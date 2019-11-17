332a5f3b874d14bf0af118fd4ef2f42d9e7f379d
hunterkiddo <hunter.kiddo@hotmail.com>
2019-11-17 13:38:08 -0500

    reverted sponsors
    grabbed code from my old commit to fix whichever commit got rid of it

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import '../style/main.css'
 
-class Sponsors extends Component {	
+class Sponsors extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
 		let sponsorsArray = []
 		sponsorsArray = createSponsors(this.props.sponsors)
@@ -14,37 +18,29 @@ class Sponsors extends Component {
 }
 
 function createSponsors(sponsors){
-	let i=0;
-	//let length = 6
-	let mnWidth=80
-	let mxWidth=(window.innerWidth/10)
-	console.log("mxw: " + mxWidth)
-	let width = (Math.min((window.innerWidth/mnWidth), (window.innerWidth/mxWidth)))
-	console.log("wide:" + width)
-	//let width = (window.innerWidth/10) + "px"
-	let length = ((window.innerWidth > 1400) ? 8 : 
-					((window.innerWidth > 900) ? 7 :
-						((window.innerWidth > 600) ? 6 :
-							(window.innerWidth > 400) ? 5 : 4)))
-	width = width + "px"
-	mxWidth = mxWidth + "px"
-	mnWidth = mnWidth + "px"
-	console.log("This width:" + width);
-	console.log("Window width:" + window.innerWidth);
-	console.log("Length:" + length)
-	let cell = []
-	let row = []
-	for(i = 0; i < sponsors.length; i++){
-		if(i % length === 0){
-			row.push(<tr> {cell} </tr>)
-			cell = []
-		}
-		cell.push(<td align='center'>
-				<a href={sponsors[i].link}> <img src={sponsors[i].source} onmouseover={sponsors[i].description} 
-				style = {{maxWidth: mxWidth, width: "auto", height:"auto", maxHeight: mxWidth}}/></a>
-			</td>)
+	let i=0
+	let sponsorsArray = []
+	for(i=0; i<sponsors.length; i++){
+		sponsorsArray.push(<div id = 'innerSponsors'>
+				<a href={sponsors[i].link}> <img id = 'sponsors' src={sponsors[i].source} onmouseover={sponsors[i].description} 
+				style = {{maxWidth: "150px", height:"auto", maxHeight: "100px", width: "auto"}}/></a>
+			</div>)
 	}
-	return row
+	return sponsorsArray
 }
 
+const Styles = {
+    container: {
+        flex: 1,
+		//backgroundColor: '#f0d03b',
+		backgroundColor: '#000',
+        width: 500,
+        height: 300,
+        flexDirection: 'row',
+        paddingLeft: 5,
+		paddingRight: 5,
+		marginLeft: 'auto',
+		marginRight: 'auto'
+    }
+}
 export { Sponsors }
