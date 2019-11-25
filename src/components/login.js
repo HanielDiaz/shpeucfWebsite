0ef59d91e16c1e84d9d27ab583d23a176f021350
Haniel Diaz <gilastomper@gmail.com>
2019-11-25 00:39:49 -0500

    moved text down to look better on deployment

--- src/components/login.js
+++ src/components/login.js
@@ -45,7 +45,7 @@ class LoginComponent extends Component {
               submitName={"Submit"}
               saveTo={(userCredentials) => this.handleSubmit(userCredentials)}
               />
-              <h1>Login</h1>
+              <h1 className="text"> Login</h1>
             </div>
         )
     }
